313ecbee741f7e415492ae5321a68ce611c7a439
GabrielFigueroa22 <Gabrielfigueroa08@aragon.unam.mx>
2022-08-28 23:35:06 -0500

    carga inicial

---
+++ src/arregloadt_1/ArregloADT_1.java
@@ -0,0 +1,45 @@
+package arregloadt_1;
+import java.util.ArrayList;
+/**
+ *
+ * @author angel
+ */
+public class ArregloADT_1 {
+    /**
+     * @param args 
+     */
+    public static void main(String[] args) {
+    }
+    private int tamanio;
+    ArrayList<Object> datos = new ArrayList(this.tamanio);
+
+    public ArregloADT_1 (int tamanio) {
+        this.tamanio = tamanio;
+        System.out.println("Dimensiones del arreglo = " + tamanio + "casillas");
+        
+        for(int i=0; i<tamanio; i++) {
+            datos.add(null);
+        }
+        System.out.println(datos);
+    }
+    public int getTamanio() {
+        return datos.lastIndexOf(datos);
+    }
+    public void setElemento(int indice, Object elemento) {
+        datos.get(indice);
+        }
+    public void limpiar(){
+        for (int i=0; i< tamanio; i++){
+            datos.set(i, null);
+        }
+    }
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        return datos.toString();
+    }
+}
+
